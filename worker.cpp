94986ed0bb118d5ca69f09128ef8f3c0e0d29a7a
johnUMSL <john.garrett718@gmail.com>
2026-02-24 19:05:45 -0600

    outputs are getting mixed

--- worker.cpp
+++ worker.cpp
@@ -1,6 +1,6 @@
 // Author: John Garrett
 // Date: 2026-02-24
-// Description: Worker process for project 2 simulated clock (read-only).
+// Description: Worker process for project 2 simulated clock.
 
 #include <iostream>
 #include <sys/ipc.h>
@@ -12,8 +12,22 @@ static const int kClockInts = 2;
 
 int main(int argc, char *argv[])
 {
-  (void)argc;
-  (void)argv;
+  if (argc < 3)
+  {
+  std::cerr << "Usage: " << argv[0] << " <maxSeconds> <maxNano>" << std::endl;
+    return 1;
+  }
+
+  int max_seconds = atoi(argv[1]);
+  int max_nanos = atoi(argv[2]);
+  if (max_seconds < 0)
+  {
+    max_seconds = 0;
+  }
+  if (max_nanos < 0)
+  {
+    max_nanos = 0;
+  }
 
   key_t shm_key = ftok("oss.cpp", 0);
   if (shm_key <= 0)
@@ -39,9 +53,53 @@ int main(int argc, char *argv[])
   int *sec = &(clock[0]);
   int *nano = &(clock[1]);
 
+  std::cout << "Worker starting, PID:" << static_cast<int>(getpid())
+            << " PPID:" << static_cast<int>(getppid()) << std::endl;
+  std::cout << "Called with:" << std::endl;
+  std::cout << "Interval: " << max_seconds << " seconds, " << max_nanos << " nanoseconds" << std::endl;
+
+  int term_sec = *sec + max_seconds;
+  int term_nano = *nano + max_nanos;
+  if (term_nano >= 1000000000)
+  {
+    term_sec += term_nano / 1000000000;
+    term_nano = term_nano % 1000000000;
+  }
+
   std::cout << "WORKER PID:" << static_cast<int>(getpid())
             << " PPID:" << static_cast<int>(getppid()) << std::endl;
-  std::cout << "WORKER: clock sec=" << *sec << " nano=" << *nano << std::endl;
+  std::cout << "SysClockS: " << *sec << " SysclockNano: " << *nano
+            << " TermTimeS: " << term_sec << " TermTimeNano: " << term_nano << std::endl;
+  std::cout << "--Just Starting" << std::endl;
+
+  int start_sec = *sec;
+  int last_reported_sec = *sec;
+  while (true)
+  {
+    int cur_sec = *sec;
+    int cur_nano = *nano;
+
+    if (cur_sec > term_sec || (cur_sec == term_sec && cur_nano >= term_nano))
+    {
+      std::cout << "WORKER PID:" << static_cast<int>(getpid())
+                << " PPID:" << static_cast<int>(getppid()) << std::endl;
+      std::cout << "SysClockS: " << cur_sec << " SysclockNano: " << cur_nano
+                << " TermTimeS: " << term_sec << " TermTimeNano: " << term_nano << std::endl;
+      std::cout << "--Terminating" << std::endl;
+      break;
+    }
+
+    if (cur_sec != last_reported_sec)
+    {
+      int elapsed = cur_sec - start_sec;
+      std::cout << "WORKER PID:" << static_cast<int>(getpid())
+                << " PPID:" << static_cast<int>(getppid()) << std::endl;
+      std::cout << "SysClockS: " << cur_sec << " SysclockNano: " << cur_nano
+                << " TermTimeS: " << term_sec << " TermTimeNano: " << term_nano << std::endl;
+      std::cout << "--" << elapsed << " seconds have passed since starting" << std::endl;
+      last_reported_sec = cur_sec;
+    }
+  }
 
   shmdt(clock);
   clock = nullptr;
